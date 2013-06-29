15a8df825715657e9f7fd2f208bef908defa37a3
Jonathan Willis <jonwillis@gmail.com>
2013-06-29 14:34:25 +0100

    Adding armory

--- AlienInvasion/EarthDefenderTests.cs
+++ AlienInvasion/EarthDefenderTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using AlienInvasion.Client;
@@ -9,14 +10,14 @@ using Rhino.Mocks;
 
 namespace AlienInvasion
 {
-	[TestFixture]
-	public class EarthDefenderTests
-	{
-		[Test]
-		public void WhenLondonIsBeingInvaded()
-		{
-			var random = new Random();
-			var weapons = new List<IDefenceWeapon>
+    [TestFixture]
+    public class EarthDefenderTests
+    {
+        [Test]
+        public void WhenLondonIsBeingInvaded()
+        {
+            var random = new Random();
+            var weapons = new List<IDefenceWeapon>
 			              	{
 			              		CreateDefenceWeapon(DefenceWeaponType.Peashooter1000Blaster),
 			              		CreateDefenceWeapon(DefenceWeaponType.Peashooter1000Blaster),
@@ -25,41 +26,62 @@ namespace AlienInvasion
 			              		CreateDefenceWeapon(DefenceWeaponType.Peashooter1000Blaster)
 			              	};
 
-			var defender = new EarthDefender();
+            var defender = new EarthDefender();
 
-			for (var wave = 1; wave <= 20; wave++)
-			{
-				var numberOfInvaders = random.Next(5) + 1;
-				var invasionWave = CreateInvasionWave(numberOfInvaders, weapons);
-				var defenceStrategy = defender.DefendEarth(invasionWave);
+            for (var wave = 1; wave <= 20; wave++)
+            {
+                var numberOfInvaders = random.Next(5) + 1;
+                var invasionWave = CreateInvasionWave(numberOfInvaders, weapons);
+                var defenceStrategy = defender.DefendEarth(invasionWave);
 
-				Assert.That(defenceStrategy.WeaponsToFireAtThisWave.Count(), Is.EqualTo(numberOfInvaders));
-			}
-		}
+                Assert.That(defenceStrategy.WeaponsToFireAtThisWave.Count(), Is.EqualTo(numberOfInvaders));
+            }
+        }
 
-		private IAlienInvasionWave CreateInvasionWave(int numberOfInvaders, IEnumerable<IDefenceWeapon> weaponsAvailable)
-		{
-			var invaders = new List<IAlienInvader>();
+        private IAlienInvasionWave CreateInvasionWave(int numberOfInvaders, IEnumerable<IDefenceWeapon> weaponsAvailable)
+        {
+            var invaders = new List<IAlienInvader>();
 
-			for (var i = 0; i < numberOfInvaders; i++)
-			{
-				var invader = MockRepository.GenerateStub<IAlienInvader>();
-				invader.Stub(x => x.Size).Return(FlyingSaucerSize.Small);
-				invaders.Add(invader);
-			}
+            for (var i = 0; i < numberOfInvaders; i++)
+            {
+                var invader = MockRepository.GenerateStub<IAlienInvader>();
+                invader.Stub(x => x.Size).Return(FlyingSaucerSize.Small);
+                invaders.Add(invader);
+            }
 
-			var invasionWave = MockRepository.GenerateStub<IAlienInvasionWave>();
-			invasionWave.Stub(x => x.WeaponsAvailableForDefence).Return(weaponsAvailable.ToArray());
-			invasionWave.Stub(x => x.AlienInvaders).Return(invaders.ToArray());
+            var invasionWave = MockRepository.GenerateStub<IAlienInvasionWave>();
+            invasionWave.Stub(x => x.WeaponsAvailableForDefence).Return(weaponsAvailable.ToArray());
+            invasionWave.Stub(x => x.AlienInvaders).Return(invaders.ToArray());
 
-			return invasionWave;
-		}
+            return invasionWave;
+        }
 
-		private IDefenceWeapon CreateDefenceWeapon(DefenceWeaponType weaponType)
-		{
-			var weapon = MockRepository.GenerateStub<IDefenceWeapon>();
-			weapon.Stub(x => x.DefenceWeaponType).Return(weaponType);
-			return weapon;
-		}
-	}
+        private IDefenceWeapon CreateDefenceWeapon(DefenceWeaponType weaponType)
+        {
+            var weapon = MockRepository.GenerateStub<IDefenceWeapon>();
+            weapon.Stub(x => x.DefenceWeaponType).Return(weaponType);
+            return weapon;
+        }
+    }
+
+    [TestFixture]
+    public class ArmoryTests
+    {
+        [TestAttribute]
+        public void NewArmoryHasNoWeapons()
+        {
+            Armory subject = new Armory();
+            CollectionAssert.IsEmpty(subject.Weapons);
+        }
+    }
+
+    public class Armory
+    {
+        public IEnumerable<IDefenceWeapon> Weapons { get; set; }
+
+        public Armory()
+        {
+            Weapons = new List<IDefenceWeapon>();
+        }
+    }
 }
