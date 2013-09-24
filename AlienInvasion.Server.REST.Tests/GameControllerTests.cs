bc71627687b71a57909f40dcf80ab4c5f430d42d
Jonathan Willis <jonathanwillis@codeweavers.net>
2013-09-24 22:12:04 +0100

    Further ideas on rest

--- AlienInvasion.Server.REST.Tests/GameControllerTests.cs
+++ AlienInvasion.Server.REST.Tests/GameControllerTests.cs
@@ -29,13 +29,6 @@ namespace AlienInvasion.Server.REST.Tests
         {
             StringAssert.EndsWith("/game/1", _result.Headers.Location.ToString());
         }
-
-        [Test]
-        public void ThenTheLocationToRegisterATeaIsPresent()
-        {
-            Assert.That(response.actions.Contains("/team/"));
-        }
-        
     }
 
     [TestFixture]
@@ -61,14 +54,30 @@ namespace AlienInvasion.Server.REST.Tests
             var createdGameid = createResponse.Headers.Location.ToString().Last().ToString();
             var response = controller.Get(createdGameid);
             Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
-            Assert.That(response.Actions["Register"].Url, Is.EqualTo("/game/"));
-            Assert.That(response.Links["Briefing"].Alt, Is.EqualTo("Next mission briefing"));
+            //Assert.That(response.Actions["Register"].Url, Is.EqualTo("/game/"));
+            //Assert.That(response.Links["Briefing"].Alt, Is.EqualTo("Next mission briefing"));
 
             //Register (Return a briefing) Links - Briefing
             //Briefing (tells you about the current mission (your current state))+ next actions URI
             //RequestAnInvasion (1st wave, list of weapons, list of aliens),  Links - SubmitADefenceStratgy
             //SubmitADefenceStratgy (Returns summary of submitted stratgy, next wave = list of weapons, list of aliens) Links - SubmitADefenceStratgy, Briefing (when completed or failed)
             //GetScoresOfAllTeams
+
+            //GET - Game - (Scoreboard) - (Links - (Register Team - POST Team)
+
+            //POST - Team - (Register - Teamname) - Location to Team/{id}
+            //GET - Team (Team Info) - Current Score, Current Level, Team name, (Link - (Briefing - GET City/London), (RequestAnInvasion - POST Invasion), (Current Wave - GET Invasion))
+
+            //POST - Invasion (Start level - TeamId) - Location to invasion/{id}
+            //GET - Invasion (Current wave info, aliens & weapons) - (Links - (Defend - POST DefenceStragey))
+
+            //POST - DefenceStragey (DefendWave - Invasion/{id}, Weapons) - Location to DefenceStragey/{id}
+            //GET - DefenceStragey (Results) - (Links - (NextWave - GET Invasion/{id}), (Finished - GET Team/{id}))  
+
+            //GET - City/{name} - (Briefing)
+            //GET - Alien/{size} - (Description)
+            //GET - Weapon/{type} - (Description)
+
         }
     }
 }
